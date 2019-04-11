079123c4ecc02d63a2b139202c9d9339bda9be39
Redmond Tran <RedmondTran@builddirect.com>
2019-04-11 14:11:32 -0700

    feat: ssr redux

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { initStore, initialCards, addItem } from '../store';
+import withRedux from 'next-redux-wrapper';
 import 'isomorphic-unfetch';
 
-export default class Index extends Component {
+class Index extends Component {
   static propTypes = {
-      stars: PropTypes.number
+      stars: PropTypes.number,
+      cards: PropTypes.array
   };
 
-  static async getInitialProps() {
-      // eslint-disable-next-line no-undef
+  static async getInitialProps({ store }) {
+      store.dispatch(initialCards());
       const res = await fetch('https://api.github.com/repos/zeit/next.js');
       const json = await res.json();
       return { stars: json.stargazers_count };
@@ -18,7 +22,23 @@ export default class Index extends Component {
       return (
           <div>
               <p>Next.js has {this.props.stars} ⭐️</p>
+              {this.props.cards.map(card => <div key={card.id}>{card.description}</div>).slice(0, 3)}
           </div>
       );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        initialCards: bindActionCreators(initialCards, dispatch),
+        addItem: bindActionCreators(addItem, dispatch)
+    };
+};
+
+const mapStateToProps = state => {
+    return {
+        cards: state.cards
+    };
+};
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Index);
